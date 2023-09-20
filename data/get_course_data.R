0d0328b39abdf2d9ff72e72fee6ac855220eedfe
Lars Schöbitz <lars@lse.de>
2023-09-20 22:23:04 +0200

    Render site

--- data/get_course_data.R
+++ data/get_course_data.R
@@ -14,15 +14,15 @@ library(dplyr)
 # course-schedule
 
 googlesheets4::read_sheet("18vF8LFHg3CBrcRqZZcE13G9UxgFXWmQwTnTUi_H8yGU") |> 
-  mutate(title = case_when(
-    is.na(page_link) == FALSE ~  paste0("[", title, "](", page_link, ")"),
-    TRUE ~ title
-  )) |> 
+  # mutate(title = case_when(
+  #   is.na(page_link) == FALSE ~  paste0("[", title, "](", page_link, ")"),
+  #   TRUE ~ title
+  # )) |> 
   write_csv(here::here("data/tbl-01-ds4owd-001-course-schedule.csv"))
 
 # learning-objectives
 
-googlesheets4::read_sheet("14znMSlTpFh9K6h46OxZznAmg6tyoNkTGX4SWN1J7NfE") |> 
+googlesheets4::read_sheet("10LvSpaCo1Hdyx0MDZKrmNXAPL-xlDyu7yMp5vroQw9Y") |> 
   write_csv(here::here("data/tbl-02-ds4owd-001-learning-objectives.csv"))
 
 
